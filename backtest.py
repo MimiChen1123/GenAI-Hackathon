e25ca28dfa03e4f19b1c0131d572158a6baee9f7
bamboo_chen <b10902005@ntu.edu.tw>
2024-09-05 13:58:20 +0800

    git rm papers.db

--- backtest.py
+++ backtest.py
@@ -2,8 +2,16 @@ from arxiv_catcher import catch_paper
 from alpha_generator import alpha_generator
 from code_generator import code_generator
 from datetime import datetime
+import importlib
+import os
+import yfinance as yf
+import shutil
 
 
+terminal_width = shutil.get_terminal_size().columns
+
+
+"""
 def isHoldingStock(today_price):
     # 讓GPT生的function，Input可以隨便(甚至可以是完整的價格的dataset)，但Output一定要是1或0
     threshold = 100
@@ -11,6 +19,7 @@ def isHoldingStock(today_price):
         return 1
     else:
         return 0
+"""
 
 
 def trade_stock(today_price, funds, stocks):
@@ -41,7 +50,6 @@ for p in price_list:
 rr = (funds + stocks * price_list[-1]) / initial_funds
 print("Return Rate = ", rr)
 
-"""
 
 # catch_paper(50)
 for i in range(100):
@@ -76,3 +84,41 @@ for i in range(100):
 
     with open(filename, 'w', encoding='utf-8') as file:
         file.write(extracted_content)
+
+"""
+
+# 文件名列表
+directory = './trading_strategy'
+file_list = []
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        file_path = os.path.join(root, file)
+        file_list.append(file_path)
+
+data = yf.download('2330.TW', start='2024-01-01', end='2024-08-31')
+price_list = data['Adj Close'].tolist()
+initial_funds = 10000
+
+for file_name in file_list:
+    print('=' * terminal_width)
+    print(file_name)
+    module = importlib.import_module(file_name)
+    StrategyClass = getattr(module, 'TradingStrategy')
+    strategy_instance = StrategyClass()
+    funds = initial_funds
+    stocks = 0
+
+    for today_price in price_list:
+        hold_stock = strategy_instance.isHoldingStock(today_price)
+
+        if hold_stock == 1:
+            stocks_to_buy = funds // today_price
+            funds -= stocks_to_buy * today_price
+            stocks += stocks_to_buy
+        else:
+            if stocks > 0:
+                funds += stocks * today_price
+                stocks = 0
+
+    rr = (funds + stocks * price_list[-1]) / initial_funds
+    print("Return Rate = ", rr)
